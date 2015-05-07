983562c656fcf417ff901f2a4aaeebc366a666f3
Azizatul Muawanah <azizatulmuawanah37@gmail.com>
2015-05-07 07:11:30 +0700

    Update RuangKelas.java

--- src/tugasbesar/RuangKelas.java
+++ src/tugasbesar/RuangKelas.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tugasbesar;
 
-/**
- *
- * @author - LENOVO -
- */
-public class RuangKelas {
-   private String namaruang;
-   private String lokasiruang;
-   private String namafakultas;
+public abstract class RuangKelas {
+   String namaruang;
+   String lokasiruang;
+   String namafakultas;
    double panjang;
    double lebar;
    double jumlahkursi;
@@ -40,232 +31,166 @@ public class RuangKelas {
    int pencahayaan;
    int kelembapan;
    int suhu;
-    
-    
-    
+   
+   public RuangKelas (String namaruang,String lokasiruang,String namafakultas,
+           double panjang,double lebar,double jumlahkursi,double jumlahjendela,
+           double jumlahpintu,double luas,double rasio,int jumlahstopkontak,
+           int kondisistopkontak,int posisistopkontak,int jumlahLCD,int kondisiLCD,
+           int posisiLCD,int jumlahlampu,int posisilampu,int jumlahkipas,
+           int posisikipas,int jumlahAC,int posisiAC,int SSID,int bandwidth,
+           int jumlahCCTV,int posisiCCTV,int sirkulasiudara,int pencahayaan,
+           int kelembapan,int suhu){
+       this.namaruang = namaruang;
+       this.lokasiruang = lokasiruang;
+       this.namafakultas = namafakultas;
+       this.panjang = panjang;
+       this.lebar = lebar;
+       this.jumlahkursi = jumlahkursi;
+       this.jumlahjendela = jumlahjendela;
+       this.jumlahpintu = jumlahpintu;
+       this.luas = luas;
+       this.rasio = rasio;
+       this.jumlahstopkontak = jumlahstopkontak;
+       this.kondisistopkontak = kondisistopkontak;
+       this.posisistopkontak = posisistopkontak;
+       this.jumlahLCD = jumlahLCD;
+       this.kondisiLCD = kondisiLCD;
+       this.posisiLCD = posisiLCD;
+       this.jumlahlampu = jumlahlampu;
+       this.posisilampu = posisilampu ;
+       this.jumlahkipas = jumlahkipas;
+       this.posisikipas = posisikipas;
+       this.jumlahAC = jumlahAC;
+       this.posisiAC = posisiAC;
+       this.SSID = SSID;
+       this.bandwidth = bandwidth;
+       this.jumlahCCTV = jumlahCCTV;
+       this.posisiCCTV = posisiCCTV;
+       this.sirkulasiudara = sirkulasiudara;
+       this.pencahayaan = pencahayaan;
+       this.kelembapan = kelembapan;
+       this.suhu = suhu;
+   }
 
     public String getNamaruang() {
         return namaruang;
-    }
-     
-    public void setNamaruang(String namaruang) {
-        this.namaruang = namaruang;
     }
 
     public String getLokasiruang() {
         return lokasiruang;
     }
 
-    public void setLokasiruang(String lokasiruang) {
-        this.lokasiruang = lokasiruang;
-    }
-
     public String getNamafakultas() {
         return namafakultas;
-    }
-
-    public void setNamafakultas(String namafakultas) {
-        this.namafakultas = namafakultas;
     }
 
     public double getPanjang() {
         return panjang;
     }
 
-    public void setPanjang(double panjang) {
-        this.panjang = panjang;
-    }
-
     public double getLebar() {
         return lebar;
-    }
-
-    public void setLebar(double lebar) {
-        this.lebar = lebar;
     }
 
     public double getJumlahkursi() {
         return jumlahkursi;
     }
 
-    public void setJumlahkursi(double jumlahkursi) {
-        this.jumlahkursi = jumlahkursi;
-    }
-
     public double getJumlahjendela() {
         return jumlahjendela;
-    }
-
-    public void setJumlahjendela(double jumlahjendela) {
-        this.jumlahjendela = jumlahjendela;
     }
 
     public double getJumlahpintu() {
         return jumlahpintu;
     }
 
-    public void setJumlahpintu(double jumlahpintu) {
-        this.jumlahpintu = jumlahpintu;
+    public double getLuas() {
+        return luas;
+    }
+
+    public double getRasio() {
+        return rasio;
     }
 
     public int getJumlahstopkontak() {
         return jumlahstopkontak;
     }
 
-    public void setJumlahstopkontak(int jumlahstopkontak) {
-        this.jumlahstopkontak = jumlahstopkontak;
-    }
-
     public int getKondisistopkontak() {
         return kondisistopkontak;
-    }
-
-    public void setKondisistopkontak(int kondisistopkontak) {
-        this.kondisistopkontak = kondisistopkontak;
     }
 
     public int getPosisistopkontak() {
         return posisistopkontak;
     }
 
-    public void setPosisistopkontak(int posisistopkontak) {
-        this.posisistopkontak = posisistopkontak;
-    }
-
     public int getJumlahLCD() {
         return jumlahLCD;
-    }
-
-    public void setJumlahLCD(int jumlahLCD) {
-        this.jumlahLCD = jumlahLCD;
     }
 
     public int getKondisiLCD() {
         return kondisiLCD;
     }
 
-    public void setKondisiLCD(int kondisiLCD) {
-        this.kondisiLCD = kondisiLCD;
-    }
-
     public int getPosisiLCD() {
         return posisiLCD;
-    }
-
-    public void setPosisiLCD(int posisiLCD) {
-        this.posisiLCD = posisiLCD;
     }
 
     public int getJumlahlampu() {
         return jumlahlampu;
     }
 
-    public void setJumlahlampu(int jumlahlampu) {
-        this.jumlahlampu = jumlahlampu;
-    }
-
     public int getPosisilampu() {
         return posisilampu;
-    }
-
-    public void setPosisilampu(int posisilampu) {
-        this.posisilampu = posisilampu;
     }
 
     public int getJumlahkipas() {
         return jumlahkipas;
     }
 
-    public void setJumlahkipas(int jumlahkipas) {
-        this.jumlahkipas = jumlahkipas;
-    }
-
     public int getPosisikipas() {
         return posisikipas;
-    }
-
-    public void setPosisikipas(int posisikipas) {
-        this.posisikipas = posisikipas;
     }
 
     public int getJumlahAC() {
         return jumlahAC;
     }
 
-    public void setJumlahAC(int jumlahAC) {
-        this.jumlahAC = jumlahAC;
-    }
-
     public int getPosisiAC() {
         return posisiAC;
-    }
-
-    public void setPosisiAC(int posisiAC) {
-        this.posisiAC = posisiAC;
     }
 
     public int getSSID() {
         return SSID;
     }
 
-    public void setSSID(int SSID) {
-        this.SSID = SSID;
-    }
-
     public int getBandwidth() {
         return bandwidth;
-    }
-
-    public void setBandwidth(int bandwidth) {
-        this.bandwidth = bandwidth;
     }
 
     public int getJumlahCCTV() {
         return jumlahCCTV;
     }
 
-    public void setJumlahCCTV(int jumlahCCTV) {
-        this.jumlahCCTV = jumlahCCTV;
-    }
-
     public int getPosisiCCTV() {
         return posisiCCTV;
-    }
-
-    public void setPosisiCCTV(int posisiCCTV) {
-        this.posisiCCTV = posisiCCTV;
     }
 
     public int getSirkulasiudara() {
         return sirkulasiudara;
     }
 
-    public void setSirkulasiudara(int sirkulasiudara) {
-        this.sirkulasiudara = sirkulasiudara;
-    }
-
     public int getPencahayaan() {
         return pencahayaan;
-    }
-
-    public void setPencahayaan(int pencahayaan) {
-        this.pencahayaan = pencahayaan;
     }
 
     public int getKelembapan() {
         return kelembapan;
     }
 
-    public void setKelembapan(int kelembapan) {
-        this.kelembapan = kelembapan;
-    }
-
     public int getSuhu() {
         return suhu;
     }
-
-    public void setSuhu(int suhu) {
-        this.suhu = suhu;
-    }
-
+    
+    public abstract void cetak() ;
 }

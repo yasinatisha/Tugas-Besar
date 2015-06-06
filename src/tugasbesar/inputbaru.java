c2fcc519d0a31e35de56f66b1b1a6f1d96f00382
yasinatisha <yasinacaca@gmail.com>
2015-06-06 14:22:41 +0700

    update final ver.1

--- src/tugasbesar/inputbaru.java
+++ src/tugasbesar/inputbaru.java
@@ -10,11 +10,11 @@ import java.sql.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-class inputbaru extends javax.swing.JFrame{
-  
-JDesktopPane desktop=new JDesktopPane(); 
+
+class inputbaru extends JFrame{
     
-    
+ JPanel panel=new JPanel();
+ 
   //InputIndentitasRuangKelas 
 JLabel lblnamaruang=new JLabel("Nama Ruang");
 JTextField txnamaruang=new JTextField(20);
@@ -151,428 +151,305 @@ setTitle("iki sek nyacak");
 setLocation(200,100);
 setSize(450,120);
 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+setLocationRelativeTo(null);
 setVisible(true);
 }
 
 void komponenVisual()
 {
-getContentPane().setLayout(null);
-//getContentPane().setLayout(new FlowLayout());
+panel.setLayout(null);
+//panel.setLayout(new FlowLayout());
 
 //InputIndentitasRuangKelas
-getContentPane().add(lblnamaruang);
+panel.add(lblnamaruang);
 //(maju,bawah,panjang,bawah/lebar)
 lblnamaruang.setBounds(10,10,120,20);
-getContentPane().add(txnamaruang);
+panel.add(txnamaruang);
 txnamaruang.setBounds(130,10,100,20);
 
-getContentPane().add(lbllokasiruang);
+panel.add(lbllokasiruang);
 lbllokasiruang.setBounds(10,30,120,20);
-getContentPane().add(txlokasiruang);
+panel.add(txlokasiruang);
 txlokasiruang.setBounds(130,30,100,20);
 
-getContentPane().add(lblnamafakultas);
+panel.add(lblnamafakultas);
 lblnamafakultas.setBounds(10,50,120,20);
-getContentPane().add(txnamafakultas);
+panel.add(txnamafakultas);
 txnamafakultas.setBounds(130,50,100,20);
 
 //InputKondisiRuangKelas
-getContentPane().add(lblpanjang);
+panel.add(lblpanjang);
 lblpanjang.setBounds(10,70,120,20);
-getContentPane().add(txpanjang);
+panel.add(txpanjang);
 txpanjang.setBounds(130,70,100,20);
-getContentPane().add(lbllebar);
+panel.add(lbllebar);
 lbllebar.setBounds(10,90,120,20);
-getContentPane().add(txlebar);
+panel.add(txlebar);
 txlebar.setBounds(130,90,100,20);
-getContentPane().add(lbljumlahpintu);
+panel.add(lbljumlahpintu);
 lbljumlahpintu.setBounds(10,110,120,20);
-getContentPane().add(txjumlahpintu);
+panel.add(txjumlahpintu);
 txjumlahpintu.setBounds(130,110,100,20);
-getContentPane().add(lbljumlahkursi);
+panel.add(lbljumlahkursi);
 lbljumlahkursi.setBounds(10,130,120,20);
-getContentPane().add(txjumlahkursi);
+panel.add(txjumlahkursi);
 txjumlahkursi.setBounds(130,130,100,20);
-getContentPane().add(lbljumlahjendela);
+panel.add(lbljumlahjendela);
 lbljumlahjendela.setBounds(10,150,120,20);
-getContentPane().add(txjumlahjendela);
+panel.add(txjumlahjendela);
 txjumlahjendela.setBounds(130,150,100,20);
 
 //InputjumlahKondisiPosisiSarana
-getContentPane().add(lbljumlahstopkontak);
+panel.add(lbljumlahstopkontak);
 lbljumlahstopkontak.setBounds(10,170,120,20);
-getContentPane().add(txjumlahstopkontak);
+panel.add(txjumlahstopkontak);
 txjumlahstopkontak.setBounds(130,170,100,20);
 
-getContentPane().add(lblkondisistopkontak);
+panel.add(lblkondisistopkontak);
 lblkondisistopkontak.setBounds(10,190,120,20);
-getContentPane().add(txkondisistopkontak);
+panel.add(txkondisistopkontak);
 txkondisistopkontak.setBounds(130,190,100,20);
 
-getContentPane().add(lblposisistopkontak);
+panel.add(lblposisistopkontak);
 lblposisistopkontak.setBounds(10,210,120,20);
-getContentPane().add(posisistopkontak1);
+panel.add(posisistopkontak1);
 posisistopkontak1.setBounds(125,200,120,43);
-getContentPane().add(posisistopkontak2);
+panel.add(posisistopkontak2);
 posisistopkontak2.setBounds(250,210,100,20);
 
-getContentPane().add(lbljumlahLCD);
+panel.add(lbljumlahLCD);
 lbljumlahLCD.setBounds(10,230,90,20);
-getContentPane().add(txjumlahLCD);
+panel.add(txjumlahLCD);
 txjumlahLCD.setBounds(130,230,100,20);
 
-getContentPane().add(lblkondisiLCD);
+panel.add(lblkondisiLCD);
 lblkondisiLCD.setBounds(10,250,120,20);
-getContentPane().add(kondisiLCD1);
+panel.add(kondisiLCD1);
 kondisiLCD1.setBounds(125,250,80,20);
-getContentPane().add(kondisiLCD2);
+panel.add(kondisiLCD2);
 kondisiLCD2.setBounds(250,250,120,20);
 
-getContentPane().add(lblposisiLCD);
+panel.add(lblposisiLCD);
 lblposisiLCD.setBounds(10,270,120,20);
-getContentPane().add(posisiLCD1);
+panel.add(posisiLCD1);
 posisiLCD1.setBounds(125,270,110,20);
-getContentPane().add(posisiLCD2);
+panel.add(posisiLCD2);
 posisiLCD2.setBounds(250,270,50,20);
 
-getContentPane().add(lbljumlahlampu);
+panel.add(lbljumlahlampu);
 lbljumlahlampu.setBounds(10,290,120,20);
-getContentPane().add(txjumlahlampu);
+panel.add(txjumlahlampu);
 txjumlahlampu.setBounds(130,290,100,20);
 
-getContentPane().add(lblposisilampu);
+panel.add(lblposisilampu);
 lblposisilampu.setBounds(10,310,120,20);
-getContentPane().add(posisilampu1);
+panel.add(posisilampu1);
 posisilampu1.setBounds(125,310,120,20);
-getContentPane().add(posisilampu2);
+panel.add(posisilampu2);
 posisilampu2.setBounds(250,310,50,20);
 
-getContentPane().add(lbljumlahkipas);
+panel.add(lbljumlahkipas);
 lbljumlahkipas.setBounds(10,330,120,20);
-getContentPane().add(txjumlahkipas);
+panel.add(txjumlahkipas);
 txjumlahkipas.setBounds(130,330,100,20);
 
-getContentPane().add(lblposisikipas);
+panel.add(lblposisikipas);
 lblposisikipas.setBounds(10,350,120,20);
-getContentPane().add(posisikipas1);
+panel.add(posisikipas1);
 posisikipas1.setBounds(125,350,120,20);
-getContentPane().add(posisikipas2);
+panel.add(posisikipas2);
 posisikipas2.setBounds(250,350,50,20);
 
-getContentPane().add(lbljumlahAC);
+panel.add(lbljumlahAC);
 lbljumlahAC.setBounds(10,370,120,20);
-getContentPane().add(txjumlahAC);
+panel.add(txjumlahAC);
 txjumlahAC.setBounds(130,370,100,20);
 
-getContentPane().add(lblposisiAC);
+panel.add(lblposisiAC);
 lblposisiAC.setBounds(10,390,120,20);
-getContentPane().add(posisiAC1);
+panel.add(posisiAC1);
 posisiAC1.setBounds(125,390,80,20);
-getContentPane().add(posisiAC2);
+panel.add(posisiAC2);
 posisiAC2.setBounds(250,390,80,20);
-getContentPane().add(posisiAC3);
+panel.add(posisiAC3);
 posisiAC3.setBounds(375,390,50,20);
 
-getContentPane().add(lblSSID);
+panel.add(lblSSID);
 lblSSID.setBounds(10,410,120,20);
-getContentPane().add(SSID1);
+panel.add(SSID1);
 SSID1.setBounds(125,410,110,20);
-getContentPane().add(SSID2);
+panel.add(SSID2);
 SSID2.setBounds(250,410,50,20);
 
-getContentPane().add(lblbandhwith);
+panel.add(lblbandhwith);
 lblbandhwith.setBounds(10,430,120,20);
-getContentPane().add(bandhwith1);
+panel.add(bandhwith1);
 bandhwith1.setBounds(125,430,100,20);
-getContentPane().add(bandhwith2);
+panel.add(bandhwith2);
 bandhwith2.setBounds(250,430,120,20);
 
-getContentPane().add(lbljumlahCCTV);
+panel.add(lbljumlahCCTV);
 lbljumlahCCTV.setBounds(10,450,120,20);
-getContentPane().add(txjumlahCCTV);
+panel.add(txjumlahCCTV);
 txjumlahCCTV.setBounds(130,450,100,20);
 
-getContentPane().add(lblposisiCCTV);
+panel.add(lblposisiCCTV);
 lblposisiCCTV.setBounds(10,470,120,20);
-getContentPane().add(posisiCCTV1);
+panel.add(posisiCCTV1);
 posisiCCTV1.setBounds(125,470,80,20);
-getContentPane().add(posisiCCTV2);
+panel.add(posisiCCTV2);
 posisiCCTV2.setBounds(250,470,90,20);
 
-getContentPane().add(lblx);
+panel.add(lblx);
 lblx.setBounds(10,490,120,20);
 
 
-getContentPane().add(lblk);
+panel.add(lblk);
 lblk.setBounds(10,510,120,20);
-//getContentPane().add(k1);
+//panel.add(k1);
 //k1.setBounds(125,490,50,20);
-//getContentPane().add(k2);
+//panel.add(k2);
 //k2.setBounds(250,490,50,20);
 //
-getContentPane().add(lbll);
+panel.add(lbll);
 lbll.setBounds(10,520,120,20);
-//getContentPane().add(l1);
+//panel.add(l1);
 //l1.setBounds(125,490,50,20);
-//getContentPane().add(l2);
+//panel.add(l2);
 //l2.setBounds(250,490,50,20);
 //
-getContentPane().add(lblm);
+panel.add(lblm);
 lblm.setBounds(10,540,120,20);
-//getContentPane().add(m1);
+//panel.add(m1);
 //m1.setBounds(125,490,50,20);
-//getContentPane().add(m2);
+//panel.add(m2);
 //m2.setBounds(250,490,50,20);
 //
-getContentPane().add(lbln);
+panel.add(lbln);
 lbln.setBounds(10,560,120,20);
-//getContentPane().add(n1);
+//panel.add(n1);
 //n1.setBounds(125,490,50,20);
-//getContentPane().add(n2);
+//panel.add(n2);
 //n2.setBounds(250,490,50,20);
 //
-getContentPane().add(lblo);
+panel.add(lblo);
 lblo.setBounds(10,580,120,20);
-//getContentPane().add(o1);
+//panel.add(o1);
 //o1.setBounds(125,490,50,20);
-//getContentPane().add(o2);
+//panel.add(o2);
 //o2.setBounds(250,490,50,20);
 
-getContentPane().add(x1);
+panel.add(x1);
 x1.setBounds(125,600,80,20);
-getContentPane().add(x2);
+panel.add(x2);
 x2.setBounds(250,600,80,20);
 
-getContentPane().add(lblsirkulasiudara);
+panel.add(lblsirkulasiudara);
 lblsirkulasiudara.setBounds(10,500,120,20);
-getContentPane().add(sirkulasiudara1);
+panel.add(sirkulasiudara1);
 sirkulasiudara1.setBounds(125,620,80,20);
-getContentPane().add(sirkulasiudara2);
+panel.add(sirkulasiudara2);
 sirkulasiudara2.setBounds(250,620,110,40);
 
-getContentPane().add(lblpencahayaan);
+panel.add(lblpencahayaan);
 lblpencahayaan.setBounds(10,640,120,20);
-getContentPane().add(txpencahayaan);
+panel.add(txpencahayaan);
 txpencahayaan.setBounds(130,640,100,20);
 
-getContentPane().add(lblkelembapan);
+panel.add(lblkelembapan);
 lblkelembapan.setBounds(10,660,120,20);
-getContentPane().add(txkelembapan);
+panel.add(txkelembapan);
 txkelembapan.setBounds(130,660,100,20);
 
-getContentPane().add(lblsuhu);
+panel.add(lblsuhu);
 lblsuhu.setBounds(10,680,90,20);
-getContentPane().add(txsuhu);
+panel.add(txsuhu);
 txsuhu.setBounds(130,680,100,20);
 
 //InputKenyamananKelas
-getContentPane().add(lbls);
+panel.add(lbls);
 lbls.setBounds(10,700,120,20);
-getContentPane().add(s1);
+panel.add(s1);
 s1.setBounds(125,700,50,20);
-getContentPane().add(s2);
+panel.add(s2);
 sirkulasiudara2.setBounds(250,700,50,20);
 
-getContentPane().add(lbly);
+panel.add(lbly);
 lbly.setBounds(10,720,120,20);
-getContentPane().add(y1);
+panel.add(y1);
 y1.setBounds(125,720,50,20);
-getContentPane().add(y2);
+panel.add(y2);
 y2.setBounds(250,720,50,20);
 
-getContentPane().add(lblz);
+panel.add(lblz);
 lblz.setBounds(10,740,120,20);
-getContentPane().add(z1);
+panel.add(z1);
 z1.setBounds(125,740,50,20);
-getContentPane().add(z2);
+panel.add(z2);
 z2.setBounds(250,740,50,20);
 
-getContentPane().add(lbla);
+panel.add(lbla);
 lblsirkulasiudara.setBounds(10,760,120,20);
-getContentPane().add(a1);
+panel.add(a1);
 a1.setBounds(125,760,50,20);
-getContentPane().add(a2);
+panel.add(a2);
 a2.setBounds(250,760,50,20);
 
-getContentPane().add(lblb);
+panel.add(lblb);
 lblb.setBounds(10,790,120,20);
-getContentPane().add(b1);
+panel.add(b1);
 b1.setBounds(125,790,50,20);
-getContentPane().add(b2);
+panel.add(b2);
 b2.setBounds(250,790,50,20);
 
 //InputKeamananKelas
-getContentPane().add(lblc);
+panel.add(lblc);
 lblc.setBounds(10,810,120,20);
-getContentPane().add(c1);
+panel.add(c1);
 c1.setBounds(125,810,50,20);
-getContentPane().add(c2);
+panel.add(c2);
 c2.setBounds(250,810,50,20);
 
-getContentPane().add(lbld);
+panel.add(lbld);
 lbld.setBounds(10,810,120,20);
-getContentPane().add(d1);
+panel.add(d1);
 d1.setBounds(125,810,50,20);
-getContentPane().add(d2);
+panel.add(d2);
 d2.setBounds(250,810,50,20);
 
-getContentPane().add(lble);
+panel.add(lble);
 lble.setBounds(10,830,120,20);
-getContentPane().add(e1);
+panel.add(e1);
 e1.setBounds(125,830,50,20);
-getContentPane().add(e2);
+panel.add(e2);
 e2.setBounds(250,830,50,20);
 
-getContentPane().add(tblinsert);
+panel.add(tblinsert);
 tblinsert.setBounds(240,10,95,20);
-getContentPane().add(tblupdate);
+panel.add(tblupdate);
 tblupdate.setBounds(240,30,95,20);
-getContentPane().add(tbldelete);
+panel.add(tbldelete);
 tbldelete.setBounds(240,50,95,20);
 
+getContentPane().add(panel);
+JScrollPane scrollPane = new JScrollPane(panel);
+getContentPane().add(scrollPane, BorderLayout.CENTER); 
 setVisible(true);
 }
+
+
 //fungsi tombol
-//void AksiReaksi()
-//{
-//tblinsert.addActionListener(new ActionListener(){
-//public void actionPerformed(ActionEvent event)
-//{
-//if (event.getSource()==tblinsert)
-//{
-//try
-//{
-//namaruang=txnamaruang.getText();
-//lokasiruang=txlokasiruang.getText();
-//namafakultas=txnamafakultas.getText();
-//
-//Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-//Connection connection = DriverManager.getConnection("jdbc:odbc:kelas;uid='aziza';pw='aziza'");
-//Statement statement = connection.createStatement();
-//String sql="insert into identitasruangkelas values ('"+namaruang+"','"+lokasiruang+"','"+namafakultas+"');";
-//statement.executeUpdate(sql);
-//statement.close();
-//connection.close();
-//System.out.println("Data telah masuk");
-//txnamaruang.setText("");
-//txnamaruang.setText("");
-//txnamaruang.requestFocus();
-//}
-//catch(Exception e)
-//{
-//System.out.println("Error :"+e);
-//}
-//}
-//}
-//});
-//
-////tombol update
-//tblupdate.addActionListener(new ActionListener()
-//{
-//    public void actionPerformed(ActionEvent event)
-//{
-//if (event.getSource()==tblupdate)
-//{
-//try
-//{
-//namaruang=txnamaruang.getText();
-//lokasiruang=txlokasiruang.getText();
-//namafakultas=txnamafakultas.getText();
-//Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-//Connection connection = DriverManager.getConnection("jdbc:odbc:kelas;uid='aziza';pw='aziza'");
-//Statement statement = connection.createStatement();
-//String sql="update identitasruangkelas set lokasiruang='"+lokasiruang+"',namafakultas='"+namafakultas+"' WHERE namaruang='"+namaruang+"'";
-//statement.executeUpdate(sql);
-//statement.close();
-//connection.close();
-//System.out.println("Data teredit");
-//txnamaruang.setText("");
-//txnamaruang.setText("");
-//txnamaruang.requestFocus();
-//}
-//catch(Exception e)
-//{
-//System.out.println("Error :"+e);
-//}
-//}
-//}
-//});
-//}
-
-//{
-//inputbaru ap=new inputbaru();
-//ap.komponenVisual();
-//ap.AksiReaksi();
-//}
-//}
-//
-
-/*private void simpanActionPerform(java.awt.event.ActionEvent evt){
-    String sql= "INSERT INTO ruangkelas('"+ txnamaruang.getText()+"', '"+ txlokasiruang.getText()+"', '"+ txnamafakultas.getText()+"', '"+ 
-            txpanjang.getText()+"', '"+ txlebar.getText()+"','"+ txjumlahkursi.getText()+"','"+ txjumlahjendela.getText()+"','"+
-            txjumlahpintu.getText()+"','"+ txjumlahstopkontak.getText()+"','"+ txkondisistopkontak.getText()+"','"+ 
-            lblposisistopkontak.getText()+"','"+ txjumlahLCD.getText()+"','"+ lblkondisiLCD.getText()+"','"+ lblposisiLCD.getText()+"','"+
-            txjumlahlampu.getText()+"', '"+ lblposisilampu.getText()+"','"+ txjumlahkipas.getText()+"','"+ lblposisikipas.getText()+"','"+
-            txjumlahAC.getText()+"','"+ lblposisiAC.getText()+"','"+ lblSSID.getText()+"','"+ lblbandhwith.getText()+"','"+ txjumlahCCTV.getText()+"','"+
-            lblposisiCCTV.getText()+"','"+ lblsirkulasiudara.getText()+"','"+ lblpencahayaan.getText()+"','"+ lblkelembapan.getText()+"','"+
-            lblsuhu.getText()+"','"+ lblx.getText()+"','"+ lbls.getText()+"','"+ lbly.getText()+"','"+ lblz.getText()+"','"+ 
-            lbla.getText()+"','"+ lblb.getText()+"','"+ lblc.getText()+"','"+ lbld.getText()+"','"+ lble.getText()+"')";
-    System.out.println(sql);
-    KONEKSI a= new KONEKSI();
-    Statement st = a.getConnection1();
-    
-   try{
-       st.executeUpdate(sql);
-       JOptionPane.showMessageDialog(null, "BERHASIL DITAMBAHKAN");
-   }catch(Exception e){
-       JOptionPane.showMessageDialog(null, "GAGAL");
-   }
-   
-   txnamaruang.setText("");
-   txlokasiruang.setText("");
-   txnamafakultas.setText("");
-   txpanjang.setText("");
-   txlebar.setText("");
-   txjumlahkursi.setText("");
-   txjumlahjendela.setText("");
-   txjumlahpintu.setText("");
-   txjumlahstopkontak.setText("");
-   txkondisistopkontak.setText("");
-   lblposisistopkontak.setText("");
-   txjumlahLCD.setText("");
-   lblkondisiLCD.setText("");
-   lblposisiLCD.setText("");
-   txjumlahlampu.setText("");
-   lblposisilampu.setText("");
-   txjumlahkipas.setText("");
-   lblposisikipas.setText("");
-   txjumlahAC.setText("");
-   lblposisiAC.setText("");
-   lblSSID.setText("");
-   lblbandhwith.setText("");
-   txjumlahCCTV.setText("");
-   lblposisiCCTV.setText("");
-   lblsirkulasiudara.setText("");
-   lblpencahayaan.setText("");
-   lblkelembapan.setText("");
-   lblsuhu.setText("");
-   lblx.setText("");
-   lbls.setText("");
-   lbly.setText("");
-   lblz.setText("");
-   lbla.setText("");
-   lblb.setText("");
-   lblc.setText("");
-   lbld.setText("");
-   lble.setText("");
-}*/
-public static void main(String args[]){
-    inputbaru ap=new inputbaru();
-ap.komponenVisual();
+void AksiReaksi()
+{
+tblinsert.addActionListener(new ActionListener(){
+public void simpanActionPerform(java.awt.event.ActionEvent event) {
+	
 }
- 
-    private void simpanActionPerform(java.awt.event.ActionEvent evt) {
-         String sql= "INSERT INTO ruangkelas('"+ txnamaruang.getText()+"', '"+ txlokasiruang.getText()+"', '"+ txnamafakultas.getText()+"', '"+ 
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        if (event.getSource()==tblinsert){
+         String sql= "INSERT INTO ruangkelas(namaruang='"+ txnamaruang.getText()+"',lokasiruang= '"+ txlokasiruang.getText()+"', '"+ txnamafakultas.getText()+"', '"+ 
             txpanjang.getText()+"', '"+ txlebar.getText()+"','"+ txjumlahkursi.getText()+"','"+ txjumlahjendela.getText()+"','"+
             txjumlahpintu.getText()+"','"+ txjumlahstopkontak.getText()+"','"+ txkondisistopkontak.getText()+"','"+ 
             lblposisistopkontak.getText()+"','"+ txjumlahLCD.getText()+"','"+ lblkondisiLCD.getText()+"','"+ lblposisiLCD.getText()+"','"+
@@ -630,4 +507,13 @@ ap.komponenVisual();
    lbld.setText("");
    lble.setText("");
     }
+    }
+});
+}
+
+public static void main(String [] args){
+inputbaru ap=new inputbaru();
+ap.komponenVisual();
+ap.AksiReaksi();
+}
 }
--- src/tugasbesar/operasi.java
+++ src/tugasbesar/operasi.java
@@ -16,41 +16,46 @@ import java.util.logging.Logger;
 
 public class operasi {
     private Connection connection;
-	private final String insertindentitasruangkelas = "INSERT INTO IndentitasRuangKelas (namaruang,lokasiruang,namafakultas) VALUES (namaruang,lokasiruang,namafakultas)";
+	private final String insertindentitasruangkelas = "INSERT INTO ruangkelas ( namaruang,lokasiruang,namafakultas,panjang,lebar,jumlahkursi,jumlahjendela,"
+                + "jumlahpintu,luas,rasio,jumlahstopkontak,kondisistopkontak,posisistopkontak,jumlahLCD,kondisiLCD,posisiLCD,jumlahlampu,posisilampu,"
+                + "jumlahkipas,posisikipas,jumlahAC,posisiAC,SSID,bandwidth,jumlahCCTV,posisiCCTV,sirkulasiudara,pencahayaan,kelembapan,suhu) VALUES (txnamaruang,txlokasiruang,txnamafakultas,"
+                + "txpanjang,txlebar,txjumlahkursi,txjumlahjendela,txjumlahpintu,txjumlahstopkontak,txkondisistopkontak,lblposisistopkontak,"
+                + "txjumlahLCD,lblkondisiLCD,lblposisiLCD,txjumlahlampu,lblposisilampu,txjumlahkipas,lblposisikipas,txjumlahAC,lblposisiAC,lblSSID,"
+                + "lblbandhwith,txjumlahCCTV,lblposisiCCTV,lblsirkulasiudara,lblpencahayaan,lblkelembapan,lblsuhu,lblx,lbls,lbly,lblz,lbla,lblb,lblc,lbld,lble)";
 	private final String selectindentitasruangkelas = "SELECT namaruang,lokasiruang FROM Indentitasruangkelas WHERE identitasruangkelas LIKE namafakultas";
 	private final String deleteindentitasruangkelas = "DELETE namaruang,lokasiruang FROM Indentitasruangkelas WHERE identitasruangkelas LIKE namafakultas";
 
 
-	private final String insertkondisiruangkelas = "INSERT INTO kondisiruangkelas (jumlahpanjang,jumlahlebar,jumlahpintu,jumlahkursi,jumlahjendela) VALUES (jumlahpanjang,jumlahlebar,jumlahpintu,jumlahkursi,jumlahjendela)";
-	private final String selectkondisiruangkelas = "SELECT jumlahpanjang,jumlahlebar,jumlahpintu,jumlahkursi FROM kondisiruangkelas WHERE kondisiruangkelas LIKE jumlahjendela";
-	private final String deletekondisiruangkelas = "DELETE SELECT jumlahpanjang,jumlahlebar,jumlahpintu,jumlahkursi FROM kondisiruangkelas WHERE kondisiruangkelas LIKE jumlahjendela";
-
-
-	private final String insertjumlahKondisiposisisarana = "INSERT INTO jumlahKondisiposisisarana (jumlahstopkontak,kondisistopkontak,posisistopkontak,jumlahLCD,kondisiLCD,posisiLCD,jumlahlampu,posisilampu,jumlahkipas,posisikipas,jumlahAC,posisiAC,SSID,bandwidth,jumlahCCTV,posisiCCTV) VALUES (jumlahstopkontak,kondisistopkontak,posisistopkontak,jumlahLCD,kondisiLCD,posisiLCD,jumlahlampu,posisilampu,jumlahkipas,posisikipas,jumlahAC,posisiAC,SSID,bandwidth,jumlahCCTV,posisiCCTV)";
-	private final String selectjumlahKondisiposisisarana = "SELECT kondisistopkontak,posisistopkontak,jumlahLCD,kondisiLCD,posisiLCD,jumlahlampu,posisilampu,jumlahkipas,posisikipas,jumlahAC,posisiAC,SSID,bandwidth,jumlahCCTV,posisiCCTV FROM jumlahkondisiposisisarana WHERE jumlahkondisiposisisarana LIKE jumlahstopkontak";
-	private final String deletejumlahKondisiposisisarana = "DELETE kondisistopkontak,posisistopkontak,jumlahLCD,kondisiLCD,posisiLCD,jumlahlampu,posisilampu,jumlahkipas,posisikipas,jumlahAC,posisiAC,SSID,bandwidth,jumlahCCTV,posisiCCTV FROM jumlahkondisiposisisarana WHERE jumlahkondisiposisisarana LIKE jumlahstopkontak";
-
-
-	private final String insertlingkungankelas = "INSERT INTO lingkungankelas (x) VALUES (x)";
-	private final String selectlingkungankelas = "SELECT x FROM lingkungankelas WHERE lingkungankelas LIKE x";
-	private final String deletelingkungankelas = "DELETE x FROM lingkungankelas WHERE lingkungankelas LIKE x";
-
-
-	private final String insertkebersihankelas = "INSERT INTO kebersihankelas(sirkulasiudara,pencahayaan,kelembapan,suhu) VALUES (sirkulasiudara,pencahayaan,kelembapan,suhu)";
-	private final String selectkebersihankelas = "SELECT pencahayaan,kelembapan,suhu FROM kebersihankelas WHERE kebersihankelas LIKE sirkulasiudara";
-	private final String deletekebersihankelas = "DELETE pencahayaan,kelembapan,suhu FROM kebersihankelas WHERE kebersihankelas LIKE sirkulasiudara";
-
-
-	private final String insertKenyamananKelas = "INSERT INTO KenyamananKelas (s,y,z,a,b) VALUES (s,y,z,a,b)";
-	private final String selectKenyamananKelas = "SELECT y,z,a,b FROM KenyamananKelas WHERE KenyamananKelas LIKE s";
-	private final String deleteKenyamananKelas = "DELETE y,z,a,b FROM KenyamananKelas WHERE KenyamananKelas LIKE s";
-
-
-	private final String insertkeamanankelas = "INSERT INTO keamanankelas (c,d,e) VALUES (c,d,e)";
-	private final String selectkeamanankelas = "SELECT d,e FROM keamanankelas WHERE keamanankelas LIKE c";
-	private final String deletekeamanankelas = "DELETE d,e FROM keamanankelas WHERE keamanankelas LIKE c";
-
-
+//	private final String insertkondisiruangkelas = "INSERT INTO kondisiruangkelas (jumlahpanjang,jumlahlebar,jumlahpintu,jumlahkursi,jumlahjendela) VALUES (jumlahpanjang,jumlahlebar,jumlahpintu,jumlahkursi,jumlahjendela)";
+//	private final String selectkondisiruangkelas = "SELECT jumlahpanjang,jumlahlebar,jumlahpintu,jumlahkursi FROM kondisiruangkelas WHERE kondisiruangkelas LIKE jumlahjendela";
+//	private final String deletekondisiruangkelas = "DELETE SELECT jumlahpanjang,jumlahlebar,jumlahpintu,jumlahkursi FROM kondisiruangkelas WHERE kondisiruangkelas LIKE jumlahjendela";
+//
+//
+//	private final String insertjumlahKondisiposisisarana = "INSERT INTO jumlahKondisiposisisarana (jumlahstopkontak,kondisistopkontak,posisistopkontak,jumlahLCD,kondisiLCD,posisiLCD,jumlahlampu,posisilampu,jumlahkipas,posisikipas,jumlahAC,posisiAC,SSID,bandwidth,jumlahCCTV,posisiCCTV) VALUES (jumlahstopkontak,kondisistopkontak,posisistopkontak,jumlahLCD,kondisiLCD,posisiLCD,jumlahlampu,posisilampu,jumlahkipas,posisikipas,jumlahAC,posisiAC,SSID,bandwidth,jumlahCCTV,posisiCCTV)";
+//	private final String selectjumlahKondisiposisisarana = "SELECT kondisistopkontak,posisistopkontak,jumlahLCD,kondisiLCD,posisiLCD,jumlahlampu,posisilampu,jumlahkipas,posisikipas,jumlahAC,posisiAC,SSID,bandwidth,jumlahCCTV,posisiCCTV FROM jumlahkondisiposisisarana WHERE jumlahkondisiposisisarana LIKE jumlahstopkontak";
+//	private final String deletejumlahKondisiposisisarana = "DELETE kondisistopkontak,posisistopkontak,jumlahLCD,kondisiLCD,posisiLCD,jumlahlampu,posisilampu,jumlahkipas,posisikipas,jumlahAC,posisiAC,SSID,bandwidth,jumlahCCTV,posisiCCTV FROM jumlahkondisiposisisarana WHERE jumlahkondisiposisisarana LIKE jumlahstopkontak";
+//
+//
+//	private final String insertlingkungankelas = "INSERT INTO lingkungankelas (x) VALUES (x)";
+//	private final String selectlingkungankelas = "SELECT x FROM lingkungankelas WHERE lingkungankelas LIKE x";
+//	private final String deletelingkungankelas = "DELETE x FROM lingkungankelas WHERE lingkungankelas LIKE x";
+//
+//
+//	private final String insertkebersihankelas = "INSERT INTO kebersihankelas(sirkulasiudara,pencahayaan,kelembapan,suhu) VALUES (sirkulasiudara,pencahayaan,kelembapan,suhu)";
+//	private final String selectkebersihankelas = "SELECT pencahayaan,kelembapan,suhu FROM kebersihankelas WHERE kebersihankelas LIKE sirkulasiudara";
+//	private final String deletekebersihankelas = "DELETE pencahayaan,kelembapan,suhu FROM kebersihankelas WHERE kebersihankelas LIKE sirkulasiudara";
+//
+//
+//	private final String insertKenyamananKelas = "INSERT INTO KenyamananKelas (s,y,z,a,b) VALUES (s,y,z,a,b)";
+//	private final String selectKenyamananKelas = "SELECT y,z,a,b FROM KenyamananKelas WHERE KenyamananKelas LIKE s";
+//	private final String deleteKenyamananKelas = "DELETE y,z,a,b FROM KenyamananKelas WHERE KenyamananKelas LIKE s";
+//
+//
+//	private final String insertkeamanankelas = "INSERT INTO keamanankelas (c,d,e) VALUES (c,d,e)";
+//	private final String selectkeamanankelas = "SELECT d,e FROM keamanankelas WHERE keamanankelas LIKE c";
+//	private final String deletekeamanankelas = "DELETE d,e FROM keamanankelas WHERE keamanankelas LIKE c";
+//
+//
 
 // tolong di tambahin exception handlingnya ya ....
         // yg try {}
